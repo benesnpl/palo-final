7e0c99e54b7bebaed81dc18f0a7d034a36073b59
Benelab <78307945+benesnpl@users.noreply.github.com>
2022-05-15 04:52:29 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -19,3 +19,30 @@ variable "subnets_cidr_public" {
 	type = list
 	default = ["10.160.102.128/25","10.160.103.128/25"]
 }
+
+variable "subnets_cidr_private" {
+	type = list
+	default = ["10.160.102.16/28","10.160.103.16/28"]
+}
+
+variable "subnets_cidr_mng" {
+	type = list
+	default = ["10.160.102.0/28","10.160.103.0/28"]
+}
+
+variable "subnets_cidr_tgw" {
+	type = list
+	default = ["10.160.102.64/28","10.160.103.64/28"]
+}
+
+
+variable "subnets_cidr_gwlb" {
+	type = list
+	default = ["10.160.102.48/28","10.160.103.48/28"]
+}
+
+variable "subnets_cidr_gwlbw" {
+	type = list
+	default = ["10.160.102.32/28","10.160.103.32/28"]
+}
+
