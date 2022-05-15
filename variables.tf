d5c0dac848c35e3da1573f25d98fd74f80bab18d
Benelab <78307945+benesnpl@users.noreply.github.com>
2022-05-15 08:59:40 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -85,3 +85,70 @@ variable "rules_outbound_private_sg" {
     }
     ]
 }
+
+variable "rules_inbound_mgmt_sg" {
+  default = [
+  
+    {
+      port = 22
+      proto = "tcp"
+      cidr_block = ["10.159.94.0/23"]
+    }
+	
+	{
+      port = 443
+      proto = "tcp"
+      cidr_block = ["10.159.94.0/23"]
+    }
+	
+	
+	{
+      port = 161
+      proto = "udp"
+      cidr_block = ["10.159.94.0/23"]
+    }
+	
+	{
+      port = 8
+      proto = "icmp"
+      cidr_block = ["10.159.94.0/23"]
+    }
+	
+	{
+      port = 22
+      proto = "tcp"
+      cidr_block = ["100.70.0.0/15"]
+    }
+	
+	{
+      port = 443
+      proto = "tcp"
+      cidr_block = ["100.70.0.0/15"]
+    }
+	
+	
+	{
+      port = 161
+      proto = "udp"
+      cidr_block = ["100.70.0.0/15"]
+    }
+	
+	{
+      port = 8
+      proto = "icmp"
+      cidr_block = ["100.70.0.0/15"]
+    }
+	
+	
+    ]
+}
+
+variable "rules_outbound_private_sg" {
+  default = [
+    {
+      port = 0
+      proto = "-1"
+      cidr_block = ["0.0.0.0/0"]
+    }
+    ]
+}
