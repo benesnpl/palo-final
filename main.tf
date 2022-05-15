10f5e9e0bc1a0edccaebd7a1108148292a20af89
Benelab <78307945+benesnpl@users.noreply.github.com>
2022-05-15 05:17:19 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -93,3 +93,10 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-main" {
    Name = join("", [var.coid, "-SecVPC"])
   }
 }
+
+resource "aws_internet_gateway" "main_igw" {
+  vpc_id = aws_vpc.main_vpc.id
+  tags = {
+    Name = join("", [var.coid, "-IGW"])
+  }
+}
