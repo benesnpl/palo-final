faffe90d2dedeac86d021d6ce56f9fcc366f7a00
Benelab <78307945+benesnpl@users.noreply.github.com>
2022-05-15 04:52:44 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,6 +16,61 @@ resource "aws_subnet" "public" {
   availability_zone = element(var.azs,count.index)
   map_public_ip_on_launch = true
   tags = {
-    Name = ("Subnet-Public-${count.index+1}")
+    Name = ("Public-AZ${count.index+1}")
+  }
+}
+
+resource "aws_subnet" "Private" {
+  count = length(var.subnets_cidr_private)
+  vpc_id = aws_vpc.main_vpc.id
+  cidr_block = element(var.subnets_cidr_private,count.index)
+  availability_zone = element(var.azs,count.index)
+  map_public_ip_on_launch = true
+  tags = {
+    Name = ("Private-AZ${count.index+1}")
+  }
+}
+
+resource "aws_subnet" "MNG" {
+  count = length(var.subnets_cidr_mng)
+  vpc_id = aws_vpc.main_vpc.id
+  cidr_block = element(var.subnets_cidr_mng,count.index)
+  availability_zone = element(var.azs,count.index)
+  map_public_ip_on_launch = true
+  tags = {
+    Name = ("MNG-AZ${count.index+1}")
+  }
+}
+
+resource "aws_subnet" "TGW" {
+  count = length(var.subnets_cidr_tgw)
+  vpc_id = aws_vpc.main_vpc.id
+  cidr_block = element(var.subnets_cidr_tgw,count.index)
+  availability_zone = element(var.azs,count.index)
+  map_public_ip_on_launch = true
+  tags = {
+    Name = ("TGW-AZ${count.index+1}")
+  }
+}
+
+resource "aws_subnet" "GWLB" {
+  count = length(var.subnets_cidr_gwlb)
+  vpc_id = aws_vpc.main_vpc.id
+  cidr_block = element(var.subnets_cidr_gwlb,count.index)
+  availability_zone = element(var.azs,count.index)
+  map_public_ip_on_launch = true
+  tags = {
+    Name = ("GWLB-AZ${count.index+1}")
+  }
+}
+
+resource "aws_subnet" "GWLBE" {
+  count = length(var.subnets_cidr_gwlbe)
+  vpc_id = aws_vpc.main_vpc.id
+  cidr_block = element(var.subnets_cidr_gwlbe,count.index)
+  availability_zone = element(var.azs,count.index)
+  map_public_ip_on_launch = true
+  tags = {
+    Name = ("GWLBE-AZ${count.index+1}")
   }
 }
