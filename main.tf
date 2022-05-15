bc15f6a4bd66ce4f9186b57073458a8b2756f848
Benelab <78307945+benesnpl@users.noreply.github.com>
2022-05-15 07:33:10 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -261,4 +261,17 @@ resource "aws_vpc_endpoint" "az1" {
   subnet_ids        = [aws_subnet.GWLBE[0].id]
   vpc_endpoint_type = aws_vpc_endpoint_service.vpc_end_serv.service_type
   vpc_id            = aws_vpc.main_vpc.id
+  tags = {
+    Name = ("Security-AZ1")
+  }
+}
+
+resource "aws_vpc_endpoint" "az2" {
+  service_name      = aws_vpc_endpoint_service.vpc_end_serv.service_name
+  subnet_ids        = [for subnet in aws_subnet.GWLBE : subnet[1].id]
+  vpc_endpoint_type = aws_vpc_endpoint_service.vpc_end_serv.service_type
+  vpc_id            = aws_vpc.main_vpc.id
+  tags = {
+    Name = ("Security-AZ2")
+  }
 }
