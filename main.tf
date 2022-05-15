2ff062d68a54b4ed68fc630b5908cc7cc258ab45
Benelab <78307945+benesnpl@users.noreply.github.com>
2022-05-15 08:48:37 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -352,3 +352,31 @@ resource "aws_route_table_association" "tgw2" {
     Name = join("", [var.coid, "-Public-sg"])
   }
 }
+
+ resource "aws_security_group" "private_sg" {
+  name        = "private_sg"
+  description = "Private SG"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  dynamic "ingress" {
+    for_each = var.rules_inbound_private_sg
+    content {
+      from_port = ingress.value["port"]
+      to_port = ingress.value["port"]
+      protocol = ingress.value["proto"]
+      cidr_blocks = ingress.value["cidr_block"]
+    }
+  }
+  dynamic "egress" {
+    for_each = var.rules_outbound_private_sg
+    content {
+      from_port = egress.value["port"]
+      to_port = egress.value["port"]
+      protocol = egress.value["proto"]
+      cidr_blocks = egress.value["cidr_block"]
+    }
+  }
+  tags = {
+    Name = join("", [var.coid, "-Private-sg"])
+  }
+} 
